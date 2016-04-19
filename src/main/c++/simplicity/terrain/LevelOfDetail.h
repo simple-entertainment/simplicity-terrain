3421def46e7e74b143f111131c8d6250838af5ed
Gary Buyn <simplegsb@gmail.com>
2016-04-19 20:06:24 +1200

    Refactored TerrainSources and hid TerrainChunks not with the map's area

---
+++ src/main/c++/simplicity/terrain/LevelOfDetail.h
@@ -0,0 +1,15 @@
+/*      _                 _ _      _ _
+ *     (_)               | (_)    (_) |
+ *  ___ _ _ __ ___  _ __ | |_  ___ _| |_ _   _
+ * / __| | '_ ` _ \| '_ \| | |/ __| | __| | | |
+ * \__ \ | | | | | | |_) | | | (__| | |_| |_| |
+ * |___/_|_| |_| |_| .__/|_|_|\___|_|\__|\__, |
+ *                 | |                    __/ |
+ *                 |_|                   |___/
+ *
+ * This file is part of simplicity. See the LICENSE file for the full license governing this code.
+ */
+#ifndef BOBS_ISLAND_LEVELOFDETAIL_H
+#define BOBS_ISLAND_LEVELOFDETAIL_H
+
+#endif //BOBS_ISLAND_LEVELOFDETAIL_H
